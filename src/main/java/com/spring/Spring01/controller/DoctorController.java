a523b70fa260277bb086c6a503bf4b5961612495
ANTIC MARKO <[antic.marko69@gmail.com](mailto:antic.marko69@gmail.com)>
2023-03-13 14:09:45 +0100

    valide sinon t'es mort

---
+++ src/main/java/com/spring/Spring01/controller/DoctorController.java
@@ -0,0 +1,25 @@
+package com.spring.Spring01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class DoctorController {
+    @GetMapping("/doctor/1")
+    @ResponseBody
+    public String call() {
+        return "William Hartnell";
+    }
+
+    @GetMapping("/doctor/10")
+    @ResponseBody
+    public String call1() {
+        return "David Tennant";
+    }
+
+    @GetMapping("/doctor/13")
+    @ResponseBody
+    public String call2() {
+        return "Jodie Whittaker";
+    }
+}
